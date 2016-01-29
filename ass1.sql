0be45a1cb3d219d23007424822427ed79ccb6b77
mynameisi <test@nowhere.com>
2016-01-29 17:38:00 +0800

    finished ass1

--- ass1.sql
+++ ass1.sql
@@ -1,12 +1,12 @@
 --this is just to practice the simpliest form of drop/create
-drop table tbl cascade constraints;
-create table tbl(name VARCHAR(10));
-insert into tbl values('zhiqiao');
-select * from tbl;
+--drop table tbl cascade constraints;
+--create table tbl(name VARCHAR(10));
+--insert into tbl values('zhiqiao');
+--select * from tbl;
 
 DROP TABLE Customer CASCADE CONSTRAINTS;
 CREATE TABLE Customer(
-  custno VARCHAR(4) NOT NULL,
+  custno VARCHAR(4) CONSTRAINT customer_pk PRIMARY KEY,
   custname VARCHAR(50) NOT NULL,
   address VARCHAR(50) NOT NULL,
   Internal CHAR(1) NOT NULL,
@@ -15,4 +15,20 @@ CREATE TABLE Customer(
   city VARCHAR(50) NOT NULL,
   state VARCHAR(4) NOT NULL,
   zip  DECIMAL(5) NOT NULL
+);
+
+DROP TABLE Facility CASCADE CONSTRAINTS;
+CREATE TABLE Facility(
+  facno VARCHAR(4),
+  facname VARCHAR(50) NOT NULL UNIQUE,
+  CONSTRAINT facility_pk PRIMARY KEY (facno)
+);
+
+DROP TABLE Location CASCADE CONSTRAINTS;
+CREATE TABLE Location(
+  locno VARCHAR(4),
+  facno VARCHAR(4) NOT NULL,
+  locname VARCHAR(50) NOT NULL,
+  CONSTRAINT location_pk PRIMARY KEY (locno),
+  CONSTRAINT location_fk FOREIGN KEY (facno) REFERENCES Facility(facno)
 );
